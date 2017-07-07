edaf4a7a72d847979f556da3f3a86c977e3e8947
Andre Merzky <andre@merzky.net>
2017-07-07 21:36:27 +0200

    add demonstrator to pupulate resource DB

--- bin/01_publish_resources.py
+++ bin/01_publish_resources.py
@@ -1,31 +1,17 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
-# This demonstrator connects to a MongoDB, creates a collection 'panda.nge',
-# and publishes information about RP pilot instances, in the following form:
-#
-# {
-#   'session' : rp.session.nge,001122', 
-#   'pmgr'    : [
-#     {  
-#       'uid'    : 'pmgr.0000',
-#       'pilots' : [
-#         { 
-#           'uid'       : 'pilot.0000', 
-#           'cores'     : 128,
-#           'submitted' : 12312768,        # seconds since epoch
-#           'started'   : null,            # not yet started
-#           'runtime'   : 60,              # minutes
-#           'state'     : 'rp.LAUNCHING',  # rp level state string
-#           'control'   : 'panda_pending'  # pickup to be confirmed
-#         }, 
-#         ...
-#       ]
-#     }
-#   ]
-# }
-#      
-#
-#
-# The 'control' entry in the pilot dict are used to coordinate ownership of the
-# respective entries.
-#
+import os
+import pymongo
+import radical.utils as ru
+
+dburl = 'mongodb://localhost/nge_resources/am'
+pwd   = os.path.dirname(__file__)
+
+if __name__ == '__main__':
+
+    mongo, db, _, _, _ = ru.mongodb_connect(str(dburl))
+
+    coll  = db['rp.session.thinkie.merzky.017354.0030']
+    coll.insert(ru.read_json('%s/01_publish_resources.json' % pwd))
+
+
